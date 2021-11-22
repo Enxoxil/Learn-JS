977692f31a078423c0d76796e196883e446d82e6
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-22 22:53:19 +0200

    practise if/else

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,38 @@
 "use strict";
 
-const numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", '');
+// const numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", '');
 
-const personalMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
-};
+// const personalMovieDB = {
+//     count : numberOfFilms,
+//     movies : {},
+//     actors : {},
+//     genres : [],
+//     privat : false
+// };
 
-const answerMovies = prompt('Последний фильм?', ''),
-      answerMoviesRate = prompt('Ваша оценка?', ''),
-      answerActors = prompt('Актёр', ''),
-      answerActorsRate = prompt('Ваша оценка?', '');
+// const answerMovies = prompt('Последний фильм?', ''),
+//       answerMoviesRate = prompt('Ваша оценка?', ''),
+//       answerActors = prompt('Актёр', ''),
+//       answerActorsRate = prompt('Ваша оценка?', '');
 
-personalMovieDB.movies[answerMovies] = answerMoviesRate;
-personalMovieDB.actors[answerActors] = answerActorsRate;
+// personalMovieDB.movies[answerMovies] = answerMoviesRate;
+// personalMovieDB.actors[answerActors] = answerActorsRate;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+//(num == 5) ? console.log('Ok!') : console.log('Error');
+const num = '4';
+switch (num) {
+    case 1: 
+        console.log('Error');
+        break;
+    case 2:
+        console.log('Error');
+        break;
+    case '4':
+        console.log('Great');
+        break;
+    default:
+        console.log('Don`t worry');
+        break;
+}
