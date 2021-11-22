94831ac19d63b047f0618ef6947c05e29b448e3f
Yehor 'PC <egoriks1994@gmail.com>
2021-11-22 20:13:23 +0200

    =-

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,24 @@
 "use strict";
-let a;
-a = 7;
-console.log(a);
+//alert('Hello');
+
+// let result = confirm("Are you here?");
+// console.log(result);
+// let answer = +prompt("еть 18?", "17");
+// console.log(answer + 5);
+
+
+// let answers = [];
+
+// answers[0] = prompt("имя?", "");
+// answers[1] = prompt("фамилия?", "");
+// answers[2] = prompt("лет?", "");
+
+// console.log(typeof(answers));
+
+// let category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+// let user = 'ivan';
+// alert(`ПРивет, ${user}`);
+
+console.log( 2 + 2 * 2 !== "6");
