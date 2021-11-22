84e9ca73c8dce89257c47fb54200a74cdbb1f0c1
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-22 22:15:41 +0200

    first js app

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+const answerMovies = prompt('Последний фильм?', ''),
+      answerMoviesRate = prompt('Ваша оценка?', ''),
+      answerActors = prompt('Актёр', ''),
+      answerActorsRate = prompt('Ваша оценка?', '');
+
+personalMovieDB.movies[answerMovies] = answerMoviesRate;
+personalMovieDB.actors[answerActors] = answerActorsRate;
+
+console.log(personalMovieDB);
