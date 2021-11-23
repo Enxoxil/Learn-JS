e2f5b1d89c77375afc0e80d9e1f1253e2c0a3569
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-23 23:31:17 +0200

    разные способы реализации цикла

--- JS/script.js
+++ JS/script.js
@@ -11,18 +11,41 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < 1; i++){
+// for (let i = 0; i < 1; i++){
+//     const   a = prompt('Последний фильм?', ''),
+//             b = +prompt('Ваша оценка?', '');
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50){
+//     // Если а не "отмена" и b не "отмена" и a не пустая строка и b не пустая строка и а < 50 символов то
+//     personalMovieDB.movies[a] = b;
+//     console.log("Done!");
+//     } else {
+//         console.log("Error");
+//         i--;
+//     }
+// }
+// let x = 0;
+// while (x < 1){
+//     const   a = prompt('Последний фильм?', ''),
+//             b = +prompt('Ваша оценка?', '');
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50){
+//     // Если а не "отмена" и b не "отмена" и a не пустая строка и b не пустая строка и а < 50 символов то
+//     personalMovieDB.movies[a] = b;
+//     console.log("Done!");
+//     } else {
+//         console.log("Error");
+//         x--;
+//     }
+//     x++;
+// }
+
+let x = 0;
+while (x < 1){
     const   a = prompt('Последний фильм?', ''),
             b = +prompt('Ваша оценка?', '');
-    if(a != null && b != null && a != '' && b != '' && a.length < 50){
-    // Если а не "отмена" и b не "отмена" и a не пустая строка и b не пустая строка и а < 50 символов то
-    personalMovieDB.movies[a] = b;
-    console.log("Done!");
-    } else {
-        console.log("Error");
-        i--;
-    }
+    (a != null && b != null && a != '' && b != '' && a.length < 50) ? personalMovieDB.movies[a] = b : x--, console.log("Error");
+    x++;
 }
+
 if (personalMovieDB.count < 10){
     console.log("Мало");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
