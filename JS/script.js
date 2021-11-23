d8e28468d07b44201cc221be887a882440d89154
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-23 22:17:07 +0200

    Lesson 15

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,37 @@
 "use strict";
 
-// const numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", '');
+const numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", '');
 
-// const personalMovieDB = {
-//     count : numberOfFilms,
-//     movies : {},
-//     actors : {},
-//     genres : [],
-//     privat : false
-// };
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
 
-// const answerMovies = prompt('Последний фильм?', ''),
-//       answerMoviesRate = prompt('Ваша оценка?', ''),
-//       answerActors = prompt('Актёр', ''),
-//       answerActorsRate = prompt('Ваша оценка?', '');
 
-// personalMovieDB.movies[answerMovies] = answerMoviesRate;
-// personalMovieDB.actors[answerActors] = answerActorsRate;
-
-// console.log(personalMovieDB);
-
-//(num == 5) ? console.log('Ok!') : console.log('Error');
-const num = '4';
-switch (num) {
-    case 1: 
-        console.log('Error');
-        break;
-    case 2:
-        console.log('Error');
-        break;
-    case '4':
-        console.log('Great');
-        break;
-    default:
-        console.log('Don`t worry');
-        break;
+for (let i = 0; i < 1; i++){
+    const   a = prompt('Последний фильм?', ''),
+            b = +prompt('Ваша оценка?', '');
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+    // Если а не "отмена" и b не "отмена" и a не пустая строка и b не пустая строка и а < 50 символов то
+    personalMovieDB.movies[a] = b;
+    console.log("Done!");
+    } else {
+        console.log("Error");
+        i--;
+    }
 }
+if (personalMovieDB.count < 10){
+    console.log("Мало");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Классический задрот");
+} else if (personalMovieDB.count >= 30){
+    console.log("Ёбаный задрот");
+} else {
+    console.log("Миша, мы всё просрали!");
+}
+console.log(personalMovieDB);
+
+
