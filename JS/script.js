135e19e595cc8d40012bcf47004ec9c5adc9840d
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-25 00:20:49 +0200

    done homework 4

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,8 @@
 
 let numberOfFilms;
 
+
+
 function start() {
     numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", '');
 
@@ -19,13 +21,19 @@ const personalMovieDB = {
     privat : false
 };
 
+
 function rememberFilms(){
-    let x = 0;
-    while (x < 1){
+    for (let i = 0; i < 1; i++){
         const   a = prompt('Последний фильм?', ''),
                 b = +prompt('Ваша оценка?', '');
-        (a != null && b != null && a != '' && b != '' && a.length < 50) ? personalMovieDB.movies[a] = b : x--, console.log("Error");
-        x++;
+        if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        // Если а не "отмена" и b не "отмена" и a не пустая строка и b не пустая строка и а < 50 символов то
+        personalMovieDB.movies[a] = b;
+        console.log("Done!");
+        } else {
+            console.log("Error");
+            i--;
+        }
     }
 }
 rememberFilms();
@@ -93,5 +101,13 @@ yourGenres();
 //         console.log("Error");
 //         x--;
 //     }
+//     x++;
+// }
+// let x = 0;
+// while (x < 1){
+//     const   a = prompt('Последний фильм?', ''),
+//             b = +prompt('Ваша оценка?', '');
+//     (a != null && b != null && a != '' && b != '' && a.length < 50) ? personalMovieDB.movies[a] = b : x--, 
+//     console.log("Error");
 //     x++;
 // }
