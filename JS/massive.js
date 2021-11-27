93a54d1eff9678843a6e61d6fef6f5041de85da3
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-28 00:25:15 +0200

    forEach

--- JS/massive.js
+++ JS/massive.js
@@ -2,8 +2,16 @@
 
 
 const arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for ( let value of arr){
+//     console.log(value);
+// }
 
+// for (let i = 0; i < arr.length; i++ ){
+//     console.log(arr[i]);
+// }
 
-for (let i = 0; i < arr.length; i++ ){
-    console.log(arr[i]);
-}
+console.log(arr.length);
+
+arr.forEach(function(item, i, arr){
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+})
