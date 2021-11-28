4c964163207cd7023eed9da3fa51a9c7c59b27df
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-28 17:57:51 +0200

    popovich lesson 1

---
+++ JS/popovich.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("out").innerHTML = "Hello!";
+document.getElementById("out").innerHTML = 2019;
+document.querySelector(".header__title").innerHTML = 5;
+let title_1 = document.querySelector("#outSec");
+title_1.innerHTML = 7;
