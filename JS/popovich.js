8fb9d0e426212d2ce3d1c2cbac6da72d7a1b6555
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-28 18:57:07 +0200

    end unit 1

--- JS/popovich.js
+++ JS/popovich.js
@@ -12,7 +12,34 @@ let firstVar = (document.getElementById("one").innerHTML = "Hello world!");
 let secondVar = (document.getElementById("two").innerHTML = 12 * 12);
 let thirdVar = (document.querySelector(".one").innerHTML = "Hello world! (2)");
 let fourthVar = (document.querySelector("span").innerHTML = "world");
-let fiveVar = (document.querySelector(".three").innerHTML =
-  "<h3>kfkfkfkf</h3>");
+let fiveVar = (document.querySelector(".three").innerHTML = "<h3>Lorem</h3>");
 let sixVar = (document.querySelector(".four").innerHTML =
   "<h4>This is H4</h4> <p>This is p</p>");
+let sevenVar = (document.querySelector(".five").innerHTML = 3.1415);
+// let eightVar = (document.querySelector(".six").innerHTML =
+//   '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">');
+let z1 = 6;
+let z2 = 3;
+
+let nineVar = (document.querySelector(".seven").innerHTML = z1 * z2);
+let y1 = 6;
+let y2 = 3;
+let x1 = "hello ";
+let x2 = 5;
+let tenVar = (document.querySelector(".eight").innerHTML = y1 / y2);
+let elevenVar = (document.querySelector(".nine").innerHTML =
+  x1 + x2 + " " + +x1);
+console.log(tenVar);
+
+let d2 = document.querySelector(".test_2");
+d2 = 5;
+console.log(d2);
+
+let divs3 = document.querySelector(".s3");
+console.log(divs3);
+divs3 = document.querySelector(".s4");
+console.log(divs3);
+divs3 = document.querySelector(".s5");
+console.log(divs3);
+
+// document.querySelector("body").innerHTML = '';
