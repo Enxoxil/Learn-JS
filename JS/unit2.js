4137d586e6308ae9bde39fd01ef8d93f9a060cbe
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-28 21:08:54 +0200

    end unit 2

--- JS/unit2.js
+++ JS/unit2.js
@@ -9,13 +9,13 @@
 //   headerInput.value = "";
 // };
 
-let a = 7, 
-    b = 9,
-    c = 7,
-    d = 9,
-    e = '3',
-    f = 0,
-    f1 = 'hello';
+// let a = 7,
+//   b = 9,
+//   c = 7,
+//   d = 9,
+//   e = "3",
+//   f = 0,
+//   f1 = "hello";
 
 // console.log(a * b);
 // console.log(c / d);
@@ -32,17 +32,64 @@ let a = 7,
 //     input.value = '';
 // }
 
-let headerInputName = document.querySelector(".header__input-name");
-let headerInputLastName = document.querySelector(".header__input-lastName");
-let button = document.querySelector(".button__input-name");
+// let headerInputName = document.querySelector(".header__input-name");
+// let headerInputLastName = document.querySelector(".header__input-lastName");
+// let button = document.querySelector(".button__input-name");
 
-button.onclick = function () {
-    let inputName = headerInputName.value;
-    let inputLastName = headerInputLastName.value;
-    console.log('Hello ' + inputName + ' ' + inputLastName + '.');
-}
+// button.onclick = function () {
+//   let inputName = headerInputName.value;
+//   let inputLastName = headerInputLastName.value;
+//   console.log("Hello " + inputName + " " + inputLastName + ".");
+// };
 
+// let firtsNumber = document.querySelector(".firstNumber");
+// let secondNumber = document.querySelector(".secondNumber");
+//  let numberButton = document.querySelector(".numberButton");
 
+// numberButton.onclick = function () {
+//   let first = +firtsNumber.value;
+//   let second = +secondNumber.value;
+//   console.log(first + second);
+//     numberButton.style.border = '2px solid red';
+// };
 
+// let style = document.querySelector(".numberButton");
+// style.style.border = '2px solid red';
 
+// let inputTest = document.querySelector(".test");
+// let inputTest2 = document.querySelector(".test2");
+// let btnTest = document.querySelector(".testbtn");
 
+// btnTest.onclick = function () {
+//     let t = inputTest.value;
+//     let t2 = inputTest2.value;
+//     t = parseInt(t);
+
+//     console.log(t + +t2);
+// }
+
+let firstName = document.querySelector(".firstName");
+let lastName = document.querySelector(".lastName");
+let age = document.querySelector(".age");
+let work = document.querySelector(".work");
+let btn = document.querySelector(".btn");
+
+btn.onclick = function () {
+  let a = firstName.value;
+  let b = lastName.value;
+  let c = age.value;
+  let d = work.value;
+  console.log(
+    "Уважаемый " +
+      a +
+      " " +
+      b +
+      ", " +
+      "ваш возраст " +
+      c +
+      " years. " +
+      "Ваша профессия: " +
+      d +
+      "."
+  );
+};
