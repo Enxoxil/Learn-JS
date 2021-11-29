a9a6a295cd569f2cd493294b8fdf06c94409a839
Yehor_Haponov <egoriks1994@gmail.com>
2021-11-29 23:19:44 +0200

    unit 3 task 4

--- JS/unit3.js
+++ JS/unit3.js
@@ -10,13 +10,24 @@ const btn = document.querySelector(".btn");
 //     }
 // };
 
+// btn.onclick = function () {
+//   let out = document.querySelector(".out");
+//   let a = document.querySelector(".input3").value;
+//   let b = document.querySelector(".input4").value;
+//   if (a > b) {
+//     out.innerHTML = a;
+//   } else {
+//     out.innerHTML = b;
+//   }
+// };
+
 btn.onclick = function () {
-  let out = document.querySelector(".out");
-  let a = document.querySelector(".input3").value;
-  let b = document.querySelector(".input4").value;
-  if (a > b) {
-    out.innerHTML = a;
-  } else {
-    out.innerHTML = b;
-  }
+    let input = +document.querySelector('.input').value;
+    let out = document.querySelector('.out');
+    console.log(input);
+    if ((2021 - input) >= 18) {
+        out.innerHTML = 1;
+    } else {
+        out.innerHTML = 0;
+    }
 };
