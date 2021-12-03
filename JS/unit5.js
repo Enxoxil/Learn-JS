ff564c0d8db0a265868134b40de69be1cefe3702
Yehor_Haponov <egoriks1994@gmail.com>
2021-12-04 00:00:10 +0200

    u5 t1

--- JS/unit5.js
+++ JS/unit5.js
@@ -1,12 +1,33 @@
 //loop
 
+// let btn = document.querySelector('.btn').onclick = () => {
+//     let r = document.querySelectorAll('input[type="radio"]');
+//     console.log(r);
+//     for (let i = 0; i < r.length; i++) {
+//         if (r[i].checked) {
+//             console.log(r[i].value);
+//         }
+//     }
+// };
 
-let btn = document.querySelector('.btn').onclick = () => {
-    let r = document.querySelectorAll('input[type="radio"]');
-    console.log(r);
-    for (let i = 0; i < r.length; i++) {
-        if (r[i].checked) {
-            console.log(r[i].value);
-        }
+//homework
+
+//t1
+let strin = "";
+let btn = (document.querySelector(".btn").onclick = () => {
+  if (strin == "") {
+    for (let i = 0; i <= 50; i++) {
+      strin += i + " ";
     }
-};
+    console.log('noclear');
+    document.querySelector(".out").innerHTML = strin;
+  } else {
+      strin = '';
+      for (let i = 0; i <= 50; i++) {
+        strin += i + " ";
+      }
+      console.log('clear');
+      document.querySelector(".out").innerHTML = strin;
+  }
+  
+});
