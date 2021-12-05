395df883744b06a3e9912394ee86663450874d52
Yehor_Haponov <egoriks1994@gmail.com>
2021-12-05 20:53:07 +0200

    u6t3

--- JS/unit6.js
+++ JS/unit6.js
@@ -1,29 +1,32 @@
 let out = document.querySelector(".out");
-let strin = '';
+let strin = "";
+
+//t3
+
+let btn = (document.querySelector(".btn").onclick = () => {
+  strin = "";
+  for (let i = 1; i <= 4; i++) {
+    for (let k = 1; k <= 3; k++) {
+      strin += "*_";
+    }
+    strin += "<br>";
+  }
+  out.innerHTML = strin;
+});
 
 //t2
-let btn = document.querySelector('.btn').onclick = () => {
-    strin = '';
-    for (let i = 1; i <= 3; i++){
-        strin += i + '<br>';
-        for (let k = 1; k <= 3; k++){
+// let btn = document.querySelector('.btn').onclick = () => {
+//     strin = '';
+//     for (let i = 1; i <= 3; i++){
+//         strin += i + '<br>';
+//         for (let k = 1; k <= 3; k++){
 
-            strin += '*_';
-        }
-        strin += '<br>';
-    }
-    out.innerHTML = strin;
-};
-
-
-
-
-
-
-
-
-
-
+//             strin += '*_';
+//         }
+//         strin += '<br>';
+//     }
+//     out.innerHTML = strin;
+// };
 
 //t1
 // let btn = (document.querySelector(".btn").onclick = () => {
