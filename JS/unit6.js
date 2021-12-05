4caca1b919066bd0fd3a9fff8c30cf9038dba882
Yehor_Haponov <egoriks1994@gmail.com>
2021-12-05 20:50:17 +0200

    u6t2

--- JS/unit6.js
+++ JS/unit6.js
@@ -1,13 +1,39 @@
 let out = document.querySelector(".out");
+let strin = '';
 
-let btn = (document.querySelector(".btn").onclick = () => {
-  let strin = "";
-  for (let i = 1; i <= 3; i++) {
-    for (let k = 1; k <= 3; k++) {
-      strin += "*";
+//t2
+let btn = document.querySelector('.btn').onclick = () => {
+    strin = '';
+    for (let i = 1; i <= 3; i++){
+        strin += i + '<br>';
+        for (let k = 1; k <= 3; k++){
+
+            strin += '*_';
+        }
+        strin += '<br>';
     }
-    strin += "_";
-  }
+    out.innerHTML = strin;
+};
 
-  out.innerHTML = strin;
-});
+
+
+
+
+
+
+
+
+
+
+//t1
+// let btn = (document.querySelector(".btn").onclick = () => {
+//   let strin = "";
+//   for (let i = 1; i <= 3; i++) {
+//     for (let k = 1; k <= 3; k++) {
+//       strin += "*";
+//     }
+//     strin += "_";
+//   }
+
+//   out.innerHTML = strin;
+// });
