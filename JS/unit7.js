8936c76a764a140e342454e8b511bd33abe2b8ab
Yehor_Haponov <egoriks1994@gmail.com>
2021-12-07 23:47:06 +0200

    u7t2

--- JS/unit7.js
+++ JS/unit7.js
@@ -1,10 +1,23 @@
-let btn = document.querySelector('.btn').onclick = f1;
-
+let btn = document.querySelector('.btn');
+let input = document.querySelector('.input');
 let out = document.querySelector('.out');
 
+//2
 
-function f1 () {
-    let input = document.querySelector('.input').value;
-    out.innerHTML = input;
-    console.log(out);
+function t2 () {
+    let value = input.value;
+    return value;
 }
+
+btn.onclick = () => {
+    out.textContent = t2();
+};
+
+
+
+//t1
+// function f1 () {
+//     let input = document.querySelector('.input').value;
+//     out.innerHTML = input;
+//     console.log(out);
+// }
