7507819995ce6c3aa054342653cc68ad36c41f87
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-11 21:39:43 +0200

    u8 t3

--- JS/unit8.js
+++ JS/unit8.js
@@ -4,18 +4,28 @@ let input = document.querySelector(".input");
 let input1 = document.querySelector(".input1");
 let str = "";
 
+//t3
 
+btn.onclick = func();
+function func() {
+  let i = 25;
+  while (i >= 7) {
+      str += i + ' ';
+      i--;
+  }
+  out.innerHTML = str;
+}
 
 //t2
 
-btn.onclick = () => {
-    let i = 0;
-    while ((i % 2 == 0) && i <= 122){
-        str += i + ' ';
-        i = i + 2;
-    }
-    out.innerHTML = str;
-};
+// btn.onclick = () => {
+//     let i = 0;
+//     while ((i % 2 == 0) && i <= 122){
+//         str += i + ' ';
+//         i = i + 2;
+//     }
+//     out.innerHTML = str;
+// };
 
 //t1
 // btn.onclick = () => {
