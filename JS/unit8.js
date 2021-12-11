99e2a034d4f5e2e54a5fb896dd5447e8fedcc0e7
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-11 21:35:44 +0200

    u8 t2

--- JS/unit8.js
+++ JS/unit8.js
@@ -1,15 +1,28 @@
-let btn = document.querySelector('.btn');
-let out = document.querySelector('.out');
-let input = document.querySelector('.input');
-let input1 = document.querySelector('.input1');
-let str = '';
-//t1 
+let btn = document.querySelector(".btn");
+let out = document.querySelector(".out");
+let input = document.querySelector(".input");
+let input1 = document.querySelector(".input1");
+let str = "";
+
+
+
+//t2
 
 btn.onclick = () => {
     let i = 0;
-    while (i <= 50) {
+    while ((i % 2 == 0) && i <= 122){
         str += i + ' ';
-        i++;
+        i = i + 2;
     }
     out.innerHTML = str;
-}
+};
+
+//t1
+// btn.onclick = () => {
+//     let i = 0;
+//     while (i <= 50) {
+//         str += i + ' ';
+//         i++;
+//     }
+//     out.innerHTML = str;
+// }
