85c5b8c8dc4943c985ecdd73a1820e6695e37568
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-12 17:52:03 +0200

    u9t4

--- JS/unit9.js
+++ JS/unit9.js
@@ -1,18 +1,34 @@
-let btn = document.querySelector(".btn");
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
 let out = document.querySelector(".out");
 let input = document.querySelector(".input");
 let input1 = document.querySelector(".input1");
 let str = "";
 
-//t1
 
-btn.onclick = () => {
-  out.innerHTML = "out";
-  out.style.width = "200px";
-  out.style.height = "40px";
-  out.style.border = "1px solid red";
-  out.style.backgroundColor = "cyan";
-  out.style.textAlign = "center";
-  out.classList.add("addedClass");
-  
-};
+//t4
+btn.onclick = function() {
+    this.classList.toggle('addedClass');
+}
+btn1.onclick = () => {
+    console.log(toggl());
+    function toggl() {
+        if (btn.classList.contains('addedClass')){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+//t1,2,3
+
+// btn.onclick = () => {
+//   out.innerHTML = "out";
+//   out.style.width = "200px";
+//   out.style.height = "40px";
+//   out.style.border = "1px solid red";
+//   out.style.textAlign = "center";
+//   out.classList.add("addedClass");
+
+// };
