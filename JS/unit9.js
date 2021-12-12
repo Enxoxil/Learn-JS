582b9cefbe902b9b20009eac26c2def8b4d7c167
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-12 21:19:25 +0200

    u9t10

--- JS/unit9.js
+++ JS/unit9.js
@@ -5,14 +5,22 @@ let input = document.querySelector(".input");
 let input1 = document.querySelector(".input1");
 let str = "";
 
+//t9,10
+
+let outs = document.querySelectorAll(".out");
+for (let i = 0; i <= outs.length; i++){
+    outs[i].onclick = function() {
+        outs[i].classList.toggle('addedClass');
+    }
+}
 //t7,8
-btn.onclick = () => {
-  let outs = document.querySelectorAll(".out");
-  for (let i = 0; i <= outs.length; i++) {
-    outs[i].classList.toggle('addedClass');
-    outs[i].innerHTML = `It's ${i} element of massive`;
-  }
-};
+// btn.onclick = () => {
+//   let outs = document.querySelectorAll(".out");
+//   for (let i = 0; i <= outs.length; i++) {
+//     outs[i].classList.toggle('addedClass');
+//     outs[i].innerHTML = `It's ${i} element of massive`;
+//   }
+// };
 
 //t6
 
