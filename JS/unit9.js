258480fc9e4810b299a02915b9d2fd2ce0817609
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-12 16:41:43 +0200

    u9t1

---
+++ JS/unit9.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector(".btn");
+let out = document.querySelector(".out");
+let input = document.querySelector(".input");
+let input1 = document.querySelector(".input1");
+let str = "";
+
+//t1
+
+btn.onclick = () => {
+  out.innerHTML = "out";
+  out.style.width = "200px";
+  out.style.height = "40px";
+  out.style.border = "1px solid red";
+  out.style.backgroundColor = 'cyan';
+  out.style.textAlign = 'center';
+};
