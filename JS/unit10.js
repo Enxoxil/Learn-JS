5f13df064c5d82ac615cf80f401e3f4999621edd
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-14 22:30:57 +0200

    u10t1

---
+++ JS/unit10.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+let out = document.querySelector(".out");
+let out1 = document.querySelector(".out1");
+let input = document.querySelector(".input");
+let input1 = document.querySelector(".input1");
+let str = " ";
+
+//t1
+let arr = [1, 2, 'ex', true];
+console.log(arr);
