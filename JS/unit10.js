dfc87afb2c69771e5c8c5e1da627b498c6012871
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-14 22:39:16 +0200

    u10t6

--- JS/unit10.js
+++ JS/unit10.js
@@ -6,12 +6,17 @@ let input = document.querySelector(".input");
 let input1 = document.querySelector(".input1");
 let str = " ";
 
+//t6
+
+let arr = ["Name", "zodiak", "month", "date"];
+
+out.innerHTML = arr;
 //t5
 
-let arr = [1, 2, 3, 4, 5, 6]
-console.log(arr[0] + arr[2] + arr[3]);
+// let arr = [1, 2, 3, 4, 5, 6]
+// console.log(arr[0] + arr[2] + arr[3]);
 
-//t1 - 4
+// //t1 - 4
 // let arr = [1, 2, 'ex', true, 5, 6, 7, 8];
 
 // str += arr[0] + ' ' + arr[3] + ' ' + arr[arr.length - 1] + ' ';
