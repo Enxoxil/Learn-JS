586a6f17d19af1f0869d54e912109d9e7bb3a4f0
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-15 22:51:02 +0200

    u11 t2

--- JS/unit11.js
+++ JS/unit11.js
@@ -7,17 +7,13 @@ let input1 = document.querySelector(".input1");
 let str = " ";
 let str1 = " ";
 
-//t1
+//t2
 
 let arr = [33, "best", 66, "best"];
-
 btn.onclick = () => {
-  if (input.value != "" && input.value != " ") {
-    arr.push(input.value);
-    console.log(input.value);
-    outer();
-  }
-
+  arr.pop();
+  console.log(arr);
+  outer();
   input.value = "";
 };
 function outer() {
@@ -27,3 +23,24 @@ function outer() {
   }
   out.innerHTML = str;
 }
+
+//t1
+
+// let arr = [33, "best", 66, "best"];
+
+// btn.onclick = () => {
+//   if (input.value != "" && input.value != " ") {
+//     arr.push(input.value);
+//     console.log(input.value);
+//     outer();
+//   }
+
+//   input.value = "";
+// };
+// function outer() {
+//   str = " ";
+//   for (let i = 0; i < arr.length; i++) {
+//     str += arr[i] + " ";
+//   }
+//   out.innerHTML = str;
+// }
