033189b444028d0fb14200492e3909cc364427ad
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-16 22:11:34 +0200

    u11 t13

--- JS/unit11.js
+++ JS/unit11.js
@@ -7,19 +7,35 @@ let input1 = document.querySelector(".input1");
 let str = "";
 let str1 = " ";
 
-//t11
-
-let arr = [2, 3, 4, 5, 6, 7];
+//t13
+let arr = [6, 0, 22, 1, 4, 76];
+let arr2 = [];
 
 btn.onclick = () => {
-  str = "";
-  let a = arr.indexOf(+input.value);
-  if (arr[a] != -1) {
-    console.log(a);
-  } else {
-    console.log(arr[i]);
+  for (let i = arr.length - 1; i >= 0; i--) {
+    rev(i);
+    console.log(arr2);
   }
 };
+function rev(i) {
+  let a = arr[i];
+  arr2.push(a);
+}
+
+//t11
+
+// let arr = [2, 3, 4, 5, 6, 7];
+
+// btn.onclick = () => {
+//   str = "";
+//   let a = arr.indexOf(+input.value);
+
+//   if (arr[a] != -1) {
+//     console.log(a);
+//   } else {
+//     console.log(input.value);
+//   }
+// };
 
 //t10
 
