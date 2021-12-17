c08e7d235cfe2e0ac4ee5bab0baf4d734c46f8c8
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-17 22:43:25 +0200

    u12 t1

---
+++ JS/unit12.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+let out = document.querySelector(".out");
+let out1 = document.querySelector(".out1");
+let input = document.querySelector(".input");
+let input1 = document.querySelector(".input1");
+let str = "";
+let str1 = " ";
+
+//t1
+let arr = [11, 22, 33, 44, 55];
+btn.onclick = () => {
+    out.innerHTML = arr[arr.indexOf(55)];
+    
+}
