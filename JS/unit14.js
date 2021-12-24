68cfc637c688bb74ed42a37c30a119c5a112cee8
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-24 22:14:56 +0200

    feat: add task 1, Set

--- JS/unit14.js
+++ JS/unit14.js
@@ -6,3 +6,21 @@ let input = document.querySelector(".input");
 let input1 = document.querySelector(".input1");
 let str = "";
 let str1 = "";
+
+// init: start task - начало проекта
+// feat: add banner - реализован новый функционал
+// fix: change layout исправление
+// refactor: change structure - без нового функционала и изменения поведения.
+// изменил форматирование, улучшил алгоритм, переложил файлы
+
+
+
+
+//t1
+
+let sat = new Set();
+btn.onclick = () => {
+  sat.add(input.value);
+  input.value = "";
+  console.log(sat);
+};
