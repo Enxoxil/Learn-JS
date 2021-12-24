1948689e32b5f6a119e3c0cf1275132a75259242
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-24 23:16:14 +0200

    feat: add check pass in set

--- JS/unit14.js
+++ JS/unit14.js
@@ -13,22 +13,47 @@ let str1 = "";
 // refactor: change structure - без нового функционала и изменения поведения.
 // изменил форматирование, улучшил алгоритм, переложил файлы
 
-//t4,t5
+//t7
 
 let set = new Set();
-btn.onclick = () => {
+
+input.onchange = () => {
   if (input.value.length != 0) {
     set.add(input.value);
-    console.log(set);
     input.value = "";
+    console.log(set.size);
   }
-  if (input1.value.length != 0) {
-    if (set.has(input1.value)) {
-      console.log(set.size);
-      console.log(set);
-    }
+
+  if (set.size > 6) {
+    btn.removeAttribute("disabled");
+    btn.onclick = () => {
+      for (let key of set) {
+        str += key + " ";
+      }
+      out.innerHTML = str;
+    };
+  }
+  if (set.size <= 6) {
+    btn.setAttribute("disabled", 0);
   }
 };
+
+//t4,t5
+
+// let set = new Set();
+// btn.onclick = () => {
+//   if (input.value.length != 0) {
+//     set.add(input.value);
+//     console.log(set);
+//     input.value = "";
+//   }
+//   if (input1.value.length != 0) {
+//     if (set.has(input1.value)) {
+//       console.log(set.size);
+//       console.log(set);
+//     }
+//   }
+// };
 
 //t3
 
