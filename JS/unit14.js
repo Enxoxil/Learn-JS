f30620bdc42f39e67ffcdca33919933b3d0fe270
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-24 22:52:10 +0200

    feat: create search in Set

--- JS/unit14.js
+++ JS/unit14.js
@@ -13,23 +13,38 @@ let str1 = "";
 // refactor: change structure - без нового функционала и изменения поведения.
 // изменил форматирование, улучшил алгоритм, переложил файлы
 
-
-//t3
+//t4
 
 let set = new Set();
 btn.onclick = () => {
-    if (input.value.length != 0){
-        set.add(input.value);
-        input.value = '';
-        console.log(set);
-    };
-    if (input1.value.length != 0){
-        set.delete(input1.value);
-        console.log(set);
-        input1.value = '';
+  if (input.value.length != 0) {
+    set.add(input.value);
+    console.log(set);
+    input.value = "";
+  }
+  if (input1.value.length != 0) {
+    if (set.has(input1.value)) {
+      console.log(true);
+      console.log(set);
     }
-}
+  }
+};
 
+//t3
+
+// let set = new Set();
+// btn.onclick = () => {
+//     if (input.value.length != 0){
+//         set.add(input.value);
+//         input.value = '';
+//         console.log(set);
+//     };
+//     if (input1.value.length != 0){
+//         set.delete(input1.value);
+//         console.log(set);
+//         input1.value = '';
+//     }
+// }
 
 //t1,t2
 
