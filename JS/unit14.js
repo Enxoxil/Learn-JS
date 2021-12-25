9901e2b4890391588fd9a256780fd39d3825ae54
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-25 20:23:45 +0200

    (t8) feat: add for of set

--- JS/unit14.js
+++ JS/unit14.js
@@ -13,30 +13,51 @@ let str1 = "";
 // refactor: change structure - без нового функционала и изменения поведения.
 // изменил форматирование, улучшил алгоритм, переложил файлы
 
+//t8
+
+const set = new Set([
+  1, 2, 3, 4, 5, 3, 4, 7, 9, 5, 7, 8, 9, 23, 45, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56,
+  4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56,
+]);
+
+
+btn.onclick = () => {
+  const arr = [];
+  let zero = 0;
+  for (let sets of set){
+    if (sets > 5){
+      arr.push(sets);
+    }
+    
+  }
+  console.log(arr);
+
+}
+
 //t7
 
-let set = new Set();
+// let set = new Set();
 
-input.onchange = () => {
-  if (input.value.length != 0) {
-    set.add(input.value);
-    input.value = "";
-    console.log(set.size);
-  }
+// input.onchange = () => {
+//   if (input.value.length != 0) {
+//     set.add(input.value);
+//     input.value = "";
+//     console.log(set.size);
+//   }
 
-  if (set.size > 6) {
-    btn.removeAttribute("disabled");
-    btn.onclick = () => {
-      for (let key of set) {
-        str += key + " ";
-      }
-      out.innerHTML = str;
-    };
-  }
-  if (set.size <= 6) {
-    btn.setAttribute("disabled", 0);
-  }
-};
+//   if (set.size > 6) {
+//     btn.removeAttribute("disabled");
+//     btn.onclick = () => {
+//       for (let key of set) {
+//         str += key + " ";
+//       }
+//       out.innerHTML = str;
+//     };
+//   }
+//   if (set.size <= 6) {
+//     btn.setAttribute("disabled", 0);
+//   }
+// };
 
 //t4,t5
 
