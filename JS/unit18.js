830962be4f13729d2607ad705ec2bb874069088d
Gosha Gaponov <egoriks1994@gmail.com>
2021-12-29 21:18:12 +0200

    (u18 t4,5) feat: forEach + eventListener = Done

--- JS/unit18.js
+++ JS/unit18.js
@@ -13,20 +13,44 @@ let str1 = "";
 // refactor: change structure - без нового функционала и изменения поведения.
 // изменил форматирование, улучшил алгоритм, переложил файлы
 
+//t4
+
+let pOut = document.querySelectorAll(".out");
+let flag = false;
+
+btn.onclick = () => {
+  pOut.forEach(function (item) {
+    item.addEventListener("click", function () {
+      func(item.getAttribute("id"));
+    });
+  });
+};
+
+function func(ide) {
+  if (flag == false) {
+    document.getElementById(`${ide}`).style.boxSizing = "border-box";
+    document.getElementById(`${ide}`).style.border = "1px solid black";
+    flag = true;
+  } else {
+    document.getElementById(`${ide}`).style.boxSizing = "border-box";
+    document.getElementById(`${ide}`).style.border = "none";
+    flag = false;
+  }
+}
 
 //t3
 
-const arr = [2, "hello", 3, "hi", 4, "Mazai"];
+// const arr = [2, "hello", 3, "hi", 4, "Mazai"];
 
-btn.onclick = () => {
-    const array = [];
-    arr.forEach( (item) => {
-        if ( typeof item == 'number'){
-            array.push(item);
-        }
-    })
-    console.log(array);
-}
+// btn.onclick = () => {
+//     const array = [];
+//     arr.forEach( (item) => {
+//         if ( typeof item == 'number'){
+//             array.push(item);
+//         }
+//     })
+//     console.log(array);
+// }
 
 //t1,t2
 
