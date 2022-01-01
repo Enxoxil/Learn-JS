d0ef9a44c9be6773f330f95e30dbd315eb53981a
Gosha Gaponov <egoriks1994@gmail.com>
2022-01-01 16:46:19 +0200

    start unit 19

---
+++ JS/unit19.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+let out = document.querySelector(".out");
+let out1 = document.querySelector(".out1");
+let input = document.querySelector(".input");
+let input1 = document.querySelector(".input1");
+let str = "";
+let str1 = "";
+
+// init: start task - начало проекта
+// feat: add banner - реализован новый функционал
+// fix: change layout исправление
+// refactor: change structure - без нового функционала и изменения поведения.
+// изменил форматирование, улучшил алгоритм, переложил файлы
+
