dd7a83853fc2258b2eec80c67a30c56c7ab67127
Gosha Gaponov <egoriks1994@gmail.com>
2022-01-02 20:57:35 +0200

    Start unit 20 - Key event

---
+++ JS/unit20.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+
+let out0 = document.querySelector("#out0");
+let out1 = document.querySelector("#out1");
+let out2 = document.querySelector("#out2");
+
+let input = document.querySelector(".input");
+let input1 = document.querySelector(".input1");
+
+let str = "";
+let str1 = "";
+
+// init: start task - начало проекта
+// feat: add banner - реализован новый функционал
+// fix: change layout исправление
+// refactor: change structure - без нового функционала и изменения поведения.
+// изменил форматирование, улучшил алгоритм, переложил файлы
+
+//t1
+
