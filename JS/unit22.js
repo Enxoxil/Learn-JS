1fbabacac674f2b5329877ae4e4630f3d28d43e4
Gosha Gaponov <egoriks1994@gmail.com>
2022-01-03 22:08:55 +0200

    (u22) try catch finally

---
+++ JS/unit22.js
@@ -0,0 +1,42 @@
+'use strict';
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+
+let out0 = document.querySelector("#out0");
+let out1 = document.querySelector("#out1");
+let out2 = document.querySelector("#out2");
+let out3 = document.querySelector(".out3");
+
+let input = document.querySelector(".input");
+let input1 = document.querySelector(".input1");
+
+let container = document.querySelector(".container");
+let str = "";
+let str1 = "";
+
+// init: start task - начало проекта
+// feat: add banner - реализован новый функционал
+// fix: change layout исправление
+// refactor: change structure - без нового функционала и изменения поведения.
+// изменил форматирование, улучшил алгоритм, переложил файлы
+
+//t3
+
+    btn.onclick = () => {
+try {
+        let s = 10;
+        str += s;
+        s + 10;
+      out3.innerHTML = str;  
+}
+catch (err) {
+    let div = document.createElement("div");
+    div.classList.add("out3");
+    container.appendChild(div);
+    console.log(str);
+    document.querySelector(".out3").innerHTML += str;
+}
+    };
+    
+
+
