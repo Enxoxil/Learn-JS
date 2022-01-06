f54f4b38bfec794a7637dfd1c968373d29fe3532
Yehor_Haponov <egoriks1994@gmail.com>
2022-01-06 23:08:46 +0200

    refactor: count замыкание

--- JS/fruitSort.js
+++ JS/fruitSort.js
@@ -1,46 +1,58 @@
 const arrFruit = [
-    "cherries",
-    "apple",
-    "banana",
-    "apple",
-    "lemon",
-    "banana",
-    "strawberry",
-    "banana",
-    "apple",
-    "strawberry",
-    "lemon",
-    "apple",
-    "strawberry",
-    "banana",
-    "apple",
-  ];
-  
-  const arr = []; // итоговый массив с фруктами
-  sortFruits(arrFruit);
-  
-  function sortFruits(fruits) {
-    const fruitNames = new Set(fruits); // получаем все названия фруктов
-    const fruitObj = {}; // фрукт : количество
-    for (let items of fruitNames) {
-      let count = 0;
-      fruits.forEach(function (item) {
-        if (item == items) {
-          count++;
-        }
-      });
-      fruitObj[items] = count;
-    }
-  
-    let keySorted = Object.keys(fruitObj).sort(function (a, b) {
-      // сортируем по количеству. Массив названий
-      return fruitObj[b] - fruitObj[a];
-    });
-    for (let key of keySorted) {
-      for (let i = 0; i < fruitObj[key]; i++) {
-        arr.push(key);
+  "cherries",
+  "apple",
+  "banana",
+  "apple",
+  "lemon",
+  "banana",
+  "strawberry",
+  "banana",
+  "apple",
+  "strawberry",
+  "lemon",
+  "apple",
+  "strawberry",
+  "banana",
+  "apple",
+  "сосиска",
+];
+
+const arr = []; // итоговый массив с фруктами
+sortFruits(arrFruit);
+
+function counter() {
+  let a = 0;
+  return function () {
+    return a++;
+  };
+}
+
+function sortFruits(fruits) {
+  const fruitNames = new Set(fruits); // получаем все названия фруктов
+  const fruitObj = {}; // фрукт : количество
+
+  for (let items of fruitNames) {
+    let b = counter();
+
+    fruits.forEach(function (item) {
+      if (item == items) {
+        b();
       }
+    });
+    fruitObj[items] = b();
+  }
+
+  let keySorted = Object.keys(fruitObj).sort(function (a, b) {
+    // сортируем по количеству. Массив названий
+    return fruitObj[b] - fruitObj[a];
+  });
+
+  for (let key of keySorted) {
+    for (let i = 0; i < fruitObj[key]; i++) {
+      arr.push(key);
     }
   }
-  
-  console.log(arr);
+  console.log(fruitObj); // итог фрукт : кол-во
+}
+
+console.log(arr); // итог
